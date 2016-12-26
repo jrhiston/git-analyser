4e68146d81bcb401d8c4f15d3803800d675f2503
Jack Histon <jrhiston@gmail.com>
2016-12-26 20:49:10 +0000

    Added build and test powershell scripts.

---
+++ src/git-analyser/test.ps1
@@ -0,0 +1,31 @@
+$PSScriptFilePath = (Get-Item $MyInvocation.MyCommand.Path).FullName
+
+" PSScriptFilePath = $PSScriptFilePath"
+
+$SolutionRoot = Split-Path -Path $PSScriptFilePath -Parent
+$ProjectJsonPath = Join-Path -Path $SolutionRoot -ChildPath "src\tests\git-analyser.tests\project.json"
+
+$DOTNET = "dotnet"
+
+& $DOTNET --info
+
+& $DOTNET restore
+
+#& $DOTNET restore "$ProjectJsonPath"
+#if (-not $?)
+#{
+#	throw "The dotnet restore process returned an error code."
+#}
+
+#& $DOTNET build "$ProjectJsonPath"
+#if (-not $?)
+#{
+#	throw "The dotnet build process returned an error code."
+#}
+
+# run them
+& $DOTNET test "$ProjectJsonPath"
+if (-not $?)
+{
+	throw "The dotnet test process returned an error code."
+}
